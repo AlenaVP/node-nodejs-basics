2e952a12762505cee2213a29419351a5bbd863d4
AlenaVP Legion <poprugo@gmail.com>
2024-10-08 14:19:24 +0200

    feat: add rename.js solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const sourceFile = path.join(__dirname, 'files', 'wrongFilename.txt');
+    const destFile = path.join(__dirname, 'files', 'properFilename.md');
+
+    try {
+        await fs.access(sourceFile);
+
+        try {
+            await fs.access(destFile);
+            throw new Error('FS operation failed');
+        } catch (error) {
+            if (error.code !== 'ENOENT') {
+                throw error;
+            }
+        }
+
+        await fs.rename(sourceFile, destFile);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
