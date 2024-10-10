a14b2b693d1bf483d9d75ef00c08b58e4d3a53e2
AlenaVP Legion <poprugo@gmail.com>
2024-10-10 10:10:27 +0200

    feat: add streams:read solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const fileReadStream = createReadStream(filePath);
+
+    fileReadStream.pipe(process.stdout);
+
+    fileReadStream.on('end', () => {
+        console.log();
+    });
+
+    fileReadStream.on('error', (err) => {
+        console.error(`Error reading file: ${err.message}`);
+    });
 };
 
 await read();
