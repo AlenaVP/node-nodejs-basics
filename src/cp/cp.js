cada749e9c279c048cbf9f89ebbf7505f2a2c9d3
AlenaVP Legion <poprugo@gmail.com>
2024-10-10 12:16:12 +0200

    feat: add solution of Child Processes task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const scriptPath = path.resolve(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [scriptPath, ...args]);
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
+    child.stderr.on('data', (data) => {
+        console.error(`Child stderr: ${data}`);
+      });
+
+      child.on('close', (code) => {
+        console.log(`Child process exited with code ${code}`);
+      });
+
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['arg1', 'arg2', 'arg3']);
