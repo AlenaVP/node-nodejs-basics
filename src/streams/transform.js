b03d45df4490b9637bc8718ddf6cdeaf1fbd0acd
AlenaVP Legion <poprugo@gmail.com>
2024-10-10 10:46:41 +0200

    feat: add streams:transform solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversedChunk = chunk.toString().split('').reverse().join('');
+            callback(null, reversedChunk);
+        }
+    });
+    process.stdin.pipe(process.stdout);
+
+    reverseTransform.on('error', (err) => {
+        console.error(`Error transforming data: ${err.message}`);
+    });
+
+    process.stdin.on('error', (err) => {
+        console.error(`Error reading from stdin: ${err.message}`);
+    });
+
+    process.stdout.on('error', (err) => {
+        console.error(`Error writing to stdout: ${err.message}`);
+    });
 };
 
 await transform();
